a33ca69569bbea6163f1047841668f5a27deb447
Robert Claus <robertclaus@gmail.com>
2018-11-17 14:40:45 -0600

    Test

---
+++ myscript.js
@@ -0,0 +1,31 @@
+console.log("My Extension Is Running!!!");
+
+
+function createPoint(){
+	var canvas = document.createElement('canvas');
+
+	canvas.id = "extensionPoint";
+	canvas.width = 5;
+	canvas.height = 5;
+	canvas.style.zIndex = 8;
+	canvas.style.position = "absolute";
+	
+	var ctx = canvas.getContext("2d");
+	ctx.beginPath();
+	ctx.arc(0,0,4,0,2*Math.PI);
+	ctx.fillStyle = "red";
+	ctx.fill();
+	ctx.stroke();
+	
+	var body = document.getElementsByTagName("body")[0];
+	body.appendChild(canvas);
+}
+
+function setPointPosition(x, y){
+	var canvas = document.getElementById('extensionPoint');
+	canvas.style.left = x + 'px';
+	canvas.style.top = y + 'px';
+}
+
+createPoint();
+setPointPosition(100,100);
